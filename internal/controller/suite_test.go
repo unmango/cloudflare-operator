00281261282d72a1fd0cfc16ce194211e4ad0790
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-14 22:59:20 -0500

    Generate the controller

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -32,6 +32,8 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	networkingv1 "k8s.io/api/networking/v1"
+
 	cloudflarev1alpha1 "github.com/unmango/cloudflare-operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
@@ -60,6 +62,9 @@ var _ = BeforeSuite(func() {
 
 	var err error
 	err = cloudflarev1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = networkingv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	// +kubebuilder:scaffold:scheme
