e97cd9d26ebcc8dfd31bf74d6acdccfd564bed4f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-18 17:31:20 -0500

    Filters schema to include only relevant types
    Updates schema bundling to produce a smaller, filtered schema file.
    
    This reduces the size of the schema by removing unused types,
    improving processing efficiency.

--- hack/filter-types.sh
+++ hack/filter-types.sh
@@ -2,4 +2,4 @@
 
 root="$(git rev-parse --show-toplevel)"
 
-jq '.components.schemas | to_entries | map(select(.key | startswith("tunnel_")))' "$root/bundled-schema.out"
+jq '.components.schemas | to_entries | map(select(.key == "dns-records_dns-record")) | from_entries' "$root/bundled-schema.out"
