17769e6567f25c07eb3d233087553d23db646b17
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-18 19:51:37 -0500

    Adds DnsRecord controller tests
    Introduces Ginkgo-based tests for the DnsRecord controller.
    
    The tests cover successful reconciliation scenarios and use gomock
    for mocking Cloudflare API interactions, improving testability and
    reliability of the controller.

--- internal/controller/dnsrecord_controller_test.go
+++ internal/controller/dnsrecord_controller_test.go
@@ -19,66 +19,99 @@ package controller
 import (
 	"context"
 
+	"github.com/cloudflare/cloudflare-go/v4"
+	"github.com/cloudflare/cloudflare-go/v4/dns"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"go.uber.org/mock/gomock"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	cloudflarev1alpha1 "github.com/unmango/cloudflare-operator/api/v1alpha1"
+	cfv1alpha1 "github.com/unmango/cloudflare-operator/api/v1alpha1"
+	"github.com/unmango/cloudflare-operator/internal/testing"
 )
 
 var _ = Describe("DnsRecord Controller", func() {
 	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+		const (
+			resourceName string = "test-resource"
+			zoneId       string = "test-zone-id"
+		)
 
 		ctx := context.Background()
 
+		var (
+			ctrl       *gomock.Controller
+			cfmock     *testing.MockClient
+			reconciler DnsRecordReconciler
+		)
+
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+			Namespace: "default",
 		}
-		dnsrecord := &cloudflarev1alpha1.DnsRecord{}
+		dnsrecord := &cfv1alpha1.DnsRecord{}
 
 		BeforeEach(func() {
-			By("creating the custom resource for the Kind DnsRecord")
-			err := k8sClient.Get(ctx, typeNamespacedName, dnsrecord)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &cloudflarev1alpha1.DnsRecord{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+			ctrl = gomock.NewController(GinkgoT())
+			cfmock = testing.NewMockClient(ctrl)
+
+			reconciler = DnsRecordReconciler{
+				Client:     k8sClient,
+				Scheme:     k8sClient.Scheme(),
+				Cloudflare: cfmock,
+			}
+			dnsrecord = &cfv1alpha1.DnsRecord{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      resourceName,
+					Namespace: "default",
+				},
+				Spec: cfv1alpha1.DnsRecordSpec{
+					ZoneId: zoneId,
+					Record: cfv1alpha1.Record{
+						ARecord: &cfv1alpha1.ARecord{
+							Name: "test-a-record",
+						},
 					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+				},
 			}
 		})
 
+		JustBeforeEach(func() {
+			Expect(k8sClient.Create(ctx, dnsrecord)).To(Succeed())
+		})
+
 		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &cloudflarev1alpha1.DnsRecord{}
+			Expect(k8sClient.Delete(ctx, dnsrecord)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			resource := &cfv1alpha1.DnsRecord{}
 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
 
 			By("Cleanup the specific resource instance DnsRecord")
 			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &DnsRecordReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+		It("should successfully reconcile the resource", func() {
+			cfmock.EXPECT().
+				CreateDnsRecord(gomock.Eq(ctx), gomock.Eq(dns.RecordNewParams{
+					ZoneID: cloudflare.F(zoneId),
+					Record: dns.ARecordParam{
+						Name: cloudflare.F("test-a-record"),
+					},
+				})).
+				Return(&dns.RecordResponse{
+					ID: "test-id",
+				}, nil)
+
+			_, err := reconciler.Reconcile(ctx, reconcile.Request{
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
 	})
 })
