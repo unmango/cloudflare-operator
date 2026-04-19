68e9392123aa8141f817f01ee178bb6a3b4c158c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-04-19 15:29:07 -0500

    Kubebuilder init script

---
+++ hack/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+kubebuilder init \
+	--domain cloudflare.unmango.dev \
+	--plugins go/v4,helm/v2-alpha \
+	--owner unmango \
+	--repo github.com/unmango/cloudflare-operator \
+	--license apache2
+
